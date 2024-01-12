cfa6331690b38de84b2ee12777b6830d89f73560
Romzik Peperomzik <WarriorWay5657@gmail.com>
2024-01-12 14:01:42 +0300

    updated itemfilter/convert_filter.py

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ AutoHotkey v1.1.34.04
 - Add DoorSearcher() to GUI, improve it functional.
 - Refactor DoDelirious() and decide do you need this fuctional or not.
 - Create GUI section for choice currency type for AlchBindScourOnce(). Split vars `alch` and `bind` with AlchBindScourRolling()?
+
+- UI
+  - Сделать возможность задавать координаты для роллеров и хоткеи для роллеров
+  - Разбить нагруженный интерфейс на табы
+  - Сделать гибкую настройку для wide/fullscreen режимов, чтобы лишь переключением радиокнопки все подтягивалось.
+- Файлы:
+  - Переименовать POE_script.ahk -> main.ahk
+  - Убрать лишние (deliriun_afk.ahk, KeyHooker.ahk ->key_hooker.ahk)
+  - Вынести глабальные переменные в отдельный файл
+  - Utils
+  - Toggles отделить тогглеры и ф-ции
+  - Вынести биндинг хоткеев к лейблам в отдельный файл
+- Убрать ненужный код и почистить отступы/комментарии
+- Добавить комментарии для неочевидных ф-ций
--- itemfilter/convert_filter.py
+++ itemfilter/convert_filter.py
@@ -1,55 +1,57 @@
 import os
 from time import sleep
 
-source_list = ["NeverSink's filter - 0-SOFT.filter",
-               "NeverSink's filter - 1-REGULAR.filter",
-               "NeverSink's filter - 2-SEMI-STRICT.filter",
-               "NeverSink's filter - 3-STRICT.filter",
-               "NeverSink's filter - 4-VERY-STRICT.filter",
-               "NeverSink's filter - 5-UBER-STRICT.filter",
-               "NeverSink's filter - 6-UBER-PLUS-STRICT.filter"
-               ]
+source_list = [
+    "NeverSink's filter - 0-SOFT.filter",
+    "NeverSink's filter - 1-REGULAR.filter",
+    "NeverSink's filter - 2-SEMI-STRICT.filter",
+    "NeverSink's filter - 3-STRICT.filter",
+    "NeverSink's filter - 4-VERY-STRICT.filter",
+    "NeverSink's filter - 5-UBER-STRICT.filter",
+    "NeverSink's filter - 6-UBER-PLUS-STRICT.filter"
+]
 
 target_suffix = "-new.filter"
 
 loot_set = "  SetBorderColor 100 0 122 255 # for quick-loot\n"
 
-loot_list = ["Class \"Maps\"", 
-             "Sockets >= 6", "LinkedSockets 6",
-             "Map Fragments",
-             "StackSize", # include Simulacrum Splinters, Perandus Coin, 
-             "Simulacrum Splinter",
-             # Currency - Regular Currency Tiering
-             "Exalted Orb", "Ancient Orb", "Blessing of Chayula", # t1exalted, t2divine
-             "Exalted Shard", "Chaos Orb", "Abrasive Catalyst", # t3annul, t4chaos
-             "Cartographer's Chisel", "Blessed Orb", # t5alchemy, t6chrom
-             "Engineer's Orb", "Chaos Shard", "Alchemy Shard", # t7chance, t8trans
-             "Fractured Fossil", "Faceted Fossil", "Aetheric Fossil", "Aberrant Fossil", # Fossil T1-T4
-             "Delirium Orb",
-             "Prime Regrading Lens", "Rogue's Marker",
-             "Tainted Oil", "Golden Oil", "Black Oil", "Azure Oil", "Amber Oil", # Oil T1-T4
-             "Deafening Essence of", "Remnant of Corruption", "Screaming Essence of", # Essence T1-T3
-             "Splinter of Chayula", "Timeless Eternal Empire Splinter", "Splinter of Esh",
-             "Class \"Incubator\"",
-             "Deregulation Scroll", "Cartographer's Seal", # Others
-             "Fated Connections", "A Dishonourable Death", "A Prodigious Hand", "A Master Seeks Help", # Prophecy T1-T3
-             "Abandoned Wealth", "A Note in the Wind", "A Familiar Call", # Divination cards T1-T3
-             "Boon of Justice", # Divination cards t4c
-             "Metamorph",
-             "Quality 20",
-             "Prime Alchemical Resonator", "Potent Chaotic Resonator", "Potent Alchemical Resonator", # Resonators T1-T3
-             "Contract", "Blueprint", "Heist Target", # Heist
-             "Chromatic Orb", "Jeweller's Orb",
-             "Artifact",
-             "Expedition Logbook", "Astragali", "Burial Medallion", "Exotic Coinage", "Scrap Metal",
-             "Grand Black Scythe Artifact", "Grand Broken Circle Artifact", "Grand Order Artifact",
-             "Greater Black Scythe Artifact", "Greater Broken Circle Artifact", "Greater Order Artifact",
-             "Greater Sun Artifact",
-             "Common Black Scythe Artifact", "Common Broken Circle Artifact", "Common Order Artifact",
-             "Common Sun Artifact", 
-             "Lesser Black Scythe Artifact", "Lesser Broken Circle Artifact","Lesser Order Artifact", 
-             "Lesser Sun Artifact"
-             ] 
+loot_list = [
+    "Class \"Maps\"", 
+    "Sockets >= 6", "LinkedSockets 6",
+    "Map Fragments",
+    "StackSize", # include Simulacrum Splinters, Perandus Coin, 
+    "Simulacrum Splinter",
+    # Currency - Regular Currency Tiering
+    "Exalted Orb", "Ancient Orb", "Blessing of Chayula", # t1exalted, t2divine
+    "Exalted Shard", "Chaos Orb", "Abrasive Catalyst", # t3annul, t4chaos
+    "Cartographer's Chisel", "Blessed Orb", # t5alchemy, t6chrom
+    "Engineer's Orb", "Chaos Shard", "Alchemy Shard", # t7chance, t8trans
+    "Fractured Fossil", "Faceted Fossil", "Aetheric Fossil", "Aberrant Fossil", # Fossil T1-T4
+    "Delirium Orb",
+    "Prime Regrading Lens", "Rogue's Marker",
+    "Tainted Oil", "Golden Oil", "Black Oil", "Azure Oil", "Amber Oil", # Oil T1-T4
+    "Deafening Essence of", "Remnant of Corruption", "Screaming Essence of", # Essence T1-T3
+    "Splinter of Chayula", "Timeless Eternal Empire Splinter", "Splinter of Esh",
+    "Class \"Incubator\"",
+    "Deregulation Scroll", "Cartographer's Seal", # Others
+    "Fated Connections", "A Dishonourable Death", "A Prodigious Hand", "A Master Seeks Help", # Prophecy T1-T3
+    "Abandoned Wealth", "A Note in the Wind", "A Familiar Call", # Divination cards T1-T3
+    "Boon of Justice", # Divination cards t4c
+    "Metamorph",
+    "Quality 20",
+    "Prime Alchemical Resonator", "Potent Chaotic Resonator", "Potent Alchemical Resonator", # Resonators T1-T3
+    "Contract", "Blueprint", "Heist Target", # Heist
+    "Chromatic Orb", "Jeweller's Orb",
+    "Artifact",
+    "Expedition Logbook", "Astragali", "Burial Medallion", "Exotic Coinage", "Scrap Metal",
+    "Grand Black Scythe Artifact", "Grand Broken Circle Artifact", "Grand Order Artifact",
+    "Greater Black Scythe Artifact", "Greater Broken Circle Artifact", "Greater Order Artifact",
+    "Greater Sun Artifact",
+    "Common Black Scythe Artifact", "Common Broken Circle Artifact", "Common Order Artifact",
+    "Common Sun Artifact", 
+    "Lesser Black Scythe Artifact", "Lesser Broken Circle Artifact","Lesser Order Artifact", 
+    "Lesser Sun Artifact"
+] 
 
 # String with Exalted tier for tests:
 # BaseType == "Albino Rhoa Feather" "Awakener's Orb" "Crusader's Exalted Orb" "Eternal Orb" "Exalted Orb" "Hunter's Exalted Orb" "Mirror of Kalandra" "Mirror Shard" "Orb of Dominance" "Prime Regrading Lens" "Redeemer's Exalted Orb" "Sacred Orb" "Secondary Regrading Lens" "Tailoring Orb" "Tainted Divine Teardrop" "Tempering Orb" "Warlord's Exalted Orb"
@@ -68,8 +70,8 @@ def convert():
         line = f.readline()
         flag = False
 
-        modifyed_filter_path = f'modifyed_filters\{target}'
-        t = open(modifyed_filter_path, "w")
+        modified_filter_path = f'modified_filters\{target}'
+        t = open(modified_filter_path, "w")
 
         while line:
             if line == "\n":
@@ -96,7 +98,7 @@ def convert():
                             break
                     t.write(line)
                     if flag:
-                       t.write(loot_set)
+                        t.write(loot_set)
                 else:
                     t.write(line)
 
